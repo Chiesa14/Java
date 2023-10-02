615507388e83c4a5b344c4098609a2abcd73d382
Chiesa14 <remychiesa14@gmail.com>
2023-10-02 10:11:33 +0200

    continue studying

--- FirstProject/src/rw/ac/rca/HelloWorld.java
+++ FirstProject/src/rw/ac/rca/HelloWorld.java
@@ -7,5 +7,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(details("Rwanda", "Mike"));
+
+        int [] numbers= {10,20,30,40,50};
+        for (int num:numbers) {
+            System.out.println(num);
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Number at i = " + i + " is " + numbers[i]);
+        }
     }
 }
