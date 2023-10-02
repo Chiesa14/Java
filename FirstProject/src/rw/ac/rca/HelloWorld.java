5c646a3165fd5d0a7ac6dc1797f5e1506557f8bf
Chiesa14 <remychiesa14@gmail.com>
2023-10-02 09:56:38 +0200

    continue studying

--- FirstProject/src/rw/ac/rca/HelloWorld.java
+++ FirstProject/src/rw/ac/rca/HelloWorld.java
@@ -1,7 +1,11 @@
 package rw.ac.rca;
 
 public class HelloWorld {
+    public static String details(String country, String firstname){
+        return firstname + " From " + country;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(details("Rwanda", "Mike"));
     }
 }
