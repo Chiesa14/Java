77484fa4d36c0c9239fc79f48458fd1ba978e6b9
Chiesa14 <remychiesa14@gmail.com>
2023-10-23 23:44:43 +0200

    Reversing files data

---
+++ FirstProject/src/rw/ac/rca/Reversing/App.java
@@ -0,0 +1,53 @@
+package rw.ac.rca.Reversing;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static String Reversing(String line) {
+        List<Character> reversedLine = new ArrayList<>();
+        for (int i = line.length() - 1; i >= 0; i--) {
+            char c = line.charAt(i);
+            reversedLine.add(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : reversedLine) {
+            sb.append(c);
+        }
+        return (String.valueOf(sb));
+    }
+
+    public static void main(String[] args) {
+        String fileInput = "input.txt";
+        String fileOutput = "output.txt";
+        List<String> lines = new ArrayList<>();
+        try {
+            File outputFile = new File(fileOutput);
+            if (outputFile.createNewFile()) {
+                System.out.println("The file Output created");
+            } else {
+                System.out.println("The output file already exists");
+            }
+            FileWriter writer = new FileWriter(outputFile);
+            BufferedWriter buff = new BufferedWriter(writer);
+            BufferedReader in = new BufferedReader(new FileReader(fileInput));
+            String str;
+            while ((str = in.readLine()) != null) {
+                lines.add(str);
+            }
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+                String reversed = Reversing(line);
+                System.out.println(reversed);
+                buff.write(reversed);
+                buff.newLine();
+            }
+            buff.close();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("exception:" + e.toString());
+        }
+    }
+}
