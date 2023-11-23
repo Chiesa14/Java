11594b6a960cc6315915c325f36bdccbf4f21848
Chiesa14 <remychiesa14@gmail.com>
2023-11-23 14:23:43 +0200

    point

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -5,27 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> dis = new ArrayList<String>();
-        dis.add("Gasabo");
-        dis.add("Rubavu");
-        dis.add(2, "Bugesera");
-        List<String> south = Arrays.asList("Muhanga", "Huye");
-        dis.addAll(south);
-        List<String> north = new ArrayList<String>();
-        north.add("Musanze");
-        north.add("Rulindo");
-        dis.addAll(3, north);
-
-        for (int i = 0; i < dis.size(); i++) {
-            System.out.println(i + "=" + dis.get(i));
-        }
-        if ((dis.contains("Musanze"))) {
-            System.out.println("Musanze has gorillas");
-        }
-        System.out.println(dis.containsAll(north));
-        String s="hello";
-        String s1 = "helol";
-        System.out.println(s.hashCode());
-        System.out.println(s1.hashCode());
+        MyPoint p = new MyPoint(12,15);
+        System.out.println(p);
     }
 }
