eee09e92807b03f2581950b068c7d4b46ec2549e
Chiesa14 <remychiesa14@gmail.com>
2023-11-23 15:19:13 +0200

    generics in java

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -5,7 +6,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MyPoint p = new MyPoint(12,15);
+        MyPoint<Integer,Integer> p = new MyPoint<Integer,Integer>(12,15);
         System.out.println(p);
+        MyPoint<Double,Double> d = new MyPoint<Double,Double>(12.5,15.8);
+        System.out.println(d);
+        d.doSomething(new ArrayList<Double>(Arrays.asList(12.5,43.7,35.2,34.6,56.8)));
+        List<MyPoint>points = new ArrayList<MyPoint>();
+        points.add(new MyPoint<Double,Float>(12.3,12.0f));
+        points.add(new MyPoint<Float,Float>(12.6f,15.6f));
+        points.add(new MyPoint<Integer,Integer>(12,15));
+        points.add(d);
+
+        System.out.println(points);
     }
 }
--- Collection/src/MyPoint.java
+++ Collection/src/MyPoint.java
@@ -1,33 +1,39 @@
-public class MyPoint {
+import java.util.ArrayList;
+
+public class MyPoint<T, R> {
+    private T x;
+    private R y;
+
     @Override
     public String toString() {
-        return "MyPoint{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return "MyPoint [" + "x=" + x + ", y=" + y + ']';
     }
 
-    private int x;
-    private int y;
 
-    public MyPoint(int x, int y) {
+    public MyPoint(T x, R y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX() {
+    public T getX() {
         return x;
     }
 
-    public void setX(int x) {
+    public void setX(T x) {
         this.x = x;
     }
 
-    public int getY() {
+    public R getY() {
         return y;
     }
 
-    public void setY(int y) {
+    public void setY(R y) {
         this.y = y;
+    }
+
+    public void doSomething(ArrayList<T> ari) {
+        for (Object i : ari) {
+            System.out.println(i);
+        }
     }
 }
